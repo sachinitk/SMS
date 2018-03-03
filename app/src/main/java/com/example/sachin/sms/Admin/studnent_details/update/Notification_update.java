4a66e9267fbb880203cd414cd562cbbd82943046
sachinitk <sachinitk@gmail.com>
2018-03-03 20:24:56 +0530

    admin side design stage 1

---
+++ app/src/main/java/com/example/sachin/sms/Admin/studnent_details/update/Notification_update.java
@@ -0,0 +1,15 @@
+package com.example.sachin.sms.Admin.studnent_details.update;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sachin.sms.R;
+
+public class Notification_update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_update);
+    }
+}
