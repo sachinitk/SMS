0a9545456f72cd3e9a6d9955bb9700f9a4e1c3cd
sachinitk <sachinitk@gmail.com>
2018-03-03 16:05:13 +0530

    Second Commit

--- app/src/main/java/com/example/sachin/sms/Admin/add_stud.java
+++ app/src/main/java/com/example/sachin/sms/Admin/add_stud.java
@@ -11,5 +11,6 @@ public class add_stud extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_stud);
+        
     }
 }
--- app/src/main/java/com/example/sachin/sms/Admin/admin_menu.java
+++ app/src/main/java/com/example/sachin/sms/Admin/admin_menu.java
@@ -1,15 +1,26 @@
 package com.example.sachin.sms.Admin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.sachin.sms.R;
 
 public class admin_menu extends AppCompatActivity {
-
+    private Button add_stud ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu);
+        add_stud = (Button)findViewById(R.id.add_student);
+
+        add_stud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(admin_menu.this, com.example.sachin.sms.Admin.add_stud.class));
+            }
+        });
     }
 }
