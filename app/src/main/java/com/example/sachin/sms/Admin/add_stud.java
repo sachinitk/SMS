c5de7cddb9c468c760edc1c18f3fbc9b0d8dca04
sachinitk <sachinitk@gmail.com>
2018-03-03 16:21:17 +0530

    third commit

--- app/src/main/java/com/example/sachin/sms/Admin/add_stud.java
+++ app/src/main/java/com/example/sachin/sms/Admin/add_stud.java
@@ -2,15 +2,24 @@ package com.example.sachin.sms.Admin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.sachin.sms.R;
 
 public class add_stud extends AppCompatActivity {
-
+    private Button subpro ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_stud);
-        
+
+        subpro = (Button)findViewById(R.id.submit_proceed1);
+        subpro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
