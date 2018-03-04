9b3381ab75a894e5f985384e199172aa9b088dce
sachinitk <sachinitk@gmail.com>
2018-03-04 12:12:17 +0530

    Dyanamically added EditText

--- app/src/main/java/com/example/sachin/sms/Admin/admin_menu.java
+++ app/src/main/java/com/example/sachin/sms/Admin/admin_menu.java
@@ -1,37 +1,68 @@
 package com.example.sachin.sms.Admin;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.sachin.sms.R;
 
 public class admin_menu extends AppCompatActivity {
     private Button add_stud ;
     private  Button update_stud;
+    private EditText et;
+    String p;
 
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String usernamef = "usernamepref";
+    SharedPreferences.Editor sharedPreferences;
+   LinearLayout ll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu);
         add_stud = (Button)findViewById(R.id.add_student);
+       ll=(LinearLayout)findViewById(R.id.ad_roll);
+       sharedPreferences = getSharedPreferences(MyPREFERENCES,Context.MODE_PRIVATE).edit();
+
         update_stud = (Button)findViewById(R.id.update_student);
-
-
         add_stud.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(admin_menu.this, com.example.sachin.sms.Admin.studnent_details.add_stud.class));
+
             }
         });
         update_stud.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(admin_menu.this, com.example.sachin.sms.Admin.studnent_details.Update_main.class));
+                et = new EditText(getApplicationContext());
+
+                ll.addView(et);
+                Button bt = new Button(getApplicationContext());
+                bt.setText("Proceed");
+                ll.addView(bt);
+                bt.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        p = et.getText().toString().trim();
+                        sharedPreferences.putString("usernamef",p);
+                        sharedPreferences.apply();;
+
+                        startActivity(new Intent(admin_menu.this, com.example.sachin.sms.Admin.studnent_details.add_stud.class));
+
+                    }
+                });
             }
         });
+       // setContentView(ll);
 
     }
 }
