18726d1b0dd10621d6bcbd6f4e56ed5a6cd5ff38
sachinitk <sachinitk@gmail.com>
2018-03-06 23:46:36 +0530

    student_main_menu is ready

---
+++ app/src/main/java/com/example/sachin/sms/Student/stu_main.java
@@ -0,0 +1,15 @@
+package com.example.sachin.sms.Student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sachin.sms.R;
+
+public class stu_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stu_main);
+    }
+}
