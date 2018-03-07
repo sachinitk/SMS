a44d027bd57bfce011a9a11e466da41268ab4dc1
sachinitk <sachinitk@gmail.com>
2018-03-07 09:50:57 +0530

    working on view basic

---
+++ app/src/main/java/com/example/sachin/sms/Student/View_basic.java
@@ -0,0 +1,15 @@
+package com.example.sachin.sms.Student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sachin.sms.R;
+
+public class View_basic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_basic);
+    }
+}
